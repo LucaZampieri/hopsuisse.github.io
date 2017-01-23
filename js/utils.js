127044af35a688bd144c5c2bf1726e11039e8d30
ochanon <ondine.chanon@gmail.com>
2017-01-23 16:37:15 +0100

    Time distribution with personal time drawn

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,3 @@
-function test() {
-    console.log("hello world")
-}
-
 function linspace(data, num, round) {
     var min = Math.min(...data)
     var max = Math.max(...data)
@@ -49,4 +45,53 @@ function valueCounts(data, bins, includeLast) {
     //console.log(counts)
 
     return counts
+}
+
+function secondsToTime(secondsString) {
+    var minutes = Math.floor(Number(secondsString) / 60)
+    var seconds = Number(secondsString) % 60
+    var hours = Math.floor(minutes / 60)
+    minutes = minutes % 60
+    if (hours > 0) {
+        return hours + ':' + minutes + ':' + seconds
+    } else {
+        return minutes + ':' + seconds
+    }
+}
+
+function getWeatherIcon(weather) {
+    var iconDict = {
+        'Partly cloudy': 'wi-cloud',
+        'Clear': 'wi-day-cloudy',
+        'Light rain shower': 'wi-day-showers',
+        'Patchy rain possible': 'wi-day-sprinkle',
+        'Mist': 'wi-day-fog',
+        'Sunny': 'wi-day-sunny',
+        'Patchy light rain with thunder': 'wi-day-storm-showers',
+        'Moderate rain': 'wi-showers',
+        'Cloudy': 'wi-cloudy',
+        'Heavy rain': 'wi-rain',
+        'Moderate snow': 'wi-day-snow',
+        'Freezing fog': 'wi-fog',
+        'Patchy light snow': 'wi-snow',
+        'Moderate or heavy snow with thunder': 'wi-day-snow-thunderstorm',
+        'Fog': 'wi-day-fog',
+        'Heavy snow': 'wi-snow',
+        'Light snow': 'wi-day-snow',
+        'Moderate or heavy rain shower': 'wi-rain',
+        'Light drizzle': 'wi-sprinkle',
+        'Patchy light rain': 'wi-showers',
+        'Moderate rain at times': 'wi-showers',
+        'Heavy rain at times': 'wi-rain',
+        'Light rain': 'wi-day-showers',
+        'Patchy light drizzle': 'wi-sprinkle',
+        'Overcast': 'wi-cloudy',
+        'Moderate or heavy sleet': 'wi-sleet',
+        'Moderate or heavy rain with thunder': 'wi-thunderstorm',
+        'Light sleet': 'wi-day-sleet',
+        'Patchy moderate snow': 'wi-day-snow',
+        'Light snow showers': 'wi-day-snow'
+    }
+    var weatherIcon = '<i class="wi ' + iconDict[weather] + '"></i>'
+    return weatherIcon
 }
